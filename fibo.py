82ab299a85cd059f0971026d79153c9f4eb88273
Martin Dvoulety <239452@vut.cz>
2023-04-24 09:29:52 +0200

    mm

--- fibo.py
+++ fibo.py
@@ -1,7 +1,18 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
 
-def main():
-    pass
+    elif n == 1:
+        return 1
+
+    else:
+        soucet = recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+        return soucet
+
+
+def main(n):
+    return recursive_nth_fibo(n)
 
 
 if __name__ == "__main__":
-    main()
+    main(7)
